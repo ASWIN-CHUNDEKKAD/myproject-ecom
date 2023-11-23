e233f3ffe931c0ca8543eb457378b2107ec6929c
ASWINDAS <cvaswin1997@gmail.com>
2023-11-23 12:06:25 +0530

    static files changing

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -143,10 +143,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = BASE_DIR / 'static'
