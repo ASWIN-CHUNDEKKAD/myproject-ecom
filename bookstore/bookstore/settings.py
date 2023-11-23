7f4c4b1d0234665078aed2d904da377e8bd15494
ASWINDAS <cvaswin1997@gmail.com>
2023-11-23 13:01:30 +0530

    xghxd

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -141,12 +141,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = '/images/'
-MEDIA_ROOT = BASE_DIR / 'static'
+# MEDIA_URL = '/images/'
+# MEDIA_ROOT = BASE_DIR / 'static'
 
 # STATIC_URL = "static/"
 # STATIC_ROOT = [os.path.join(BASE_DIR, "static")]
