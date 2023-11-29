5612d95c405ca353a002f2a414f116b1ebdded26
ASWINDAS <cvaswin1997@gmail.com>
2023-11-29 11:07:18 +0530

    debug change

--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -28,8 +28,8 @@ urlpatterns = [
     path('',include('store.urls'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
     
 # if settings.DEBUG:
 #     # import debug_toolbar
